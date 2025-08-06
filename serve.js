1efc30679a3a34975a8dea0918e2dc2cb55881ee
rodrigo <r.de.oliveira.porto@accenture.com>
2025-08-05 21:55:57 -0300

    Criação do arquivo server.js e estruturação do projeto

--- serve.js
+++ serve.js
@@ -1,1 +1,8 @@
-'use strict'
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () =>{
+  console.log("Rodando na porta ", port)
+})
